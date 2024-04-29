43481c7c370498be79685807be5881f1e1c70a06
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-04-29 19:10:53 +0530

    Fix the live preview bug

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -4,63 +4,63 @@ import {
   type MetaFunction,
   type LoaderFunctionArgs,
   type LoaderFunction,
-} from "@remix-run/node";
-import { getStoryblokApi } from "@storyblok/react";
-import { useStoryblokData } from "~/hooks";
-import { implementSeo, getPostCardData, invariantResponse } from "~/utils";
-import type { PostStoryblok } from "~/types";
-import { useParams } from "@remix-run/react";
-import { GeneralErrorBoundary } from "~/components/GeneralErrorBoundary";
-import { NotFoundPage } from "~/components/NotFoundPage";
-import { cacheControl } from "~/utils/cacheControl";
+} from '@remix-run/node'
+import { getStoryblokApi } from '@storyblok/react'
+import { useStoryblokData } from '~/hooks'
+import { implementSeo, getPostCardData, invariantResponse } from '~/utils'
+import type { PostStoryblok } from '~/types'
+import { useParams } from '@remix-run/react'
+import { GeneralErrorBoundary } from '~/components/GeneralErrorBoundary'
+import { NotFoundPage } from '~/components/NotFoundPage'
+import { cacheControl } from '~/utils/cacheControl'
 
 export const loader: LoaderFunction = async ({
   params,
   request,
 }: LoaderFunctionArgs) => {
-  let slug = params["*"] ?? "home";
-  let url = new URL(request.url);
-  url = url.href; // This gives you the full URL string
+  let slug = params['*'] ?? 'home'
+  let url = new URL(request.url)
+  url = url.href // This gives you the full URL string
 
-  const sbApi = getStoryblokApi();
+  const sbApi = getStoryblokApi()
 
-  const resolveRelations = ["post.categories"];
+  const resolveRelations = ['post.categories']
 
   const { data }: { data: any } = await sbApi
     .get(`cdn/stories/${slug}`, {
-      version: "draft",
+      version: 'draft',
     })
     .catch((e) => {
-      console.log("e", e);
-      return { data: null };
-    });
+      console.log('e', e)
+      return { data: null }
+    })
   invariantResponse(data, `there is no page with slug ${slug}`, {
     status: 404,
-  });
+  })
   let headers = {
     ...cacheControl,
-  };
+  }
   const numberOfPosts = data.story.content.body?.find(
-    (item: { component: string }) => item.component === "last-posts"
-  )?.number_of_posts;
+    (item: { component: string }) => item.component === 'last-posts'
+  )?.number_of_posts
 
   const { data: lastPosts } = await sbApi.get(`cdn/stories`, {
-    version: "draft",
-    starts_with: "blog/",
+    version: 'draft',
+    starts_with: 'blog/',
     per_page: numberOfPosts,
     is_startpage: false,
     resolve_relations: resolveRelations,
-  });
-  const story = data?.story;
+  })
+  const story = data?.story
 
   const seo = story?.content?.seo_plugin?.title
     ? story?.content?.seo_plugin
-    : story?.content?.seo[0];
-  const noFollow = story?.content?.seo[0]?.no_follow;
+    : story?.content?.seo[0]
+  const noFollow = story?.content?.seo[0]?.no_follow
 
   return json(
     {
-      blok: story.content,
+      blok: story,
       name: story.name,
       lastPosts: lastPosts?.stories?.map((p: PostStoryblok) =>
         getPostCardData(p)
@@ -70,42 +70,42 @@ export const loader: LoaderFunction = async ({
       noFollow,
     },
     { headers }
-  );
-};
+  )
+}
 
 export let headers: HeadersFunction = ({ loaderHeaders }) => {
-  return { "Cache-Control": loaderHeaders.get("Cache-Control") };
-};
+  return { 'Cache-Control': loaderHeaders.get('Cache-Control') }
+}
 
 export const meta: MetaFunction = ({ data }: { data: any }) => {
   if (!data) {
-    return [];
+    return []
   }
   return [
     ...implementSeo(data?.seo, data?.name),
     {
-      "script:ld+json": {
-        "@context": "https://schema.org",
-        "@type": "WebPage",
+      'script:ld+json': {
+        '@context': 'https://schema.org',
+        '@type': 'WebPage',
         url: `${data.url}`,
       },
     },
     data.noFollow && {
-      name: "robots",
-      content: "noindex, nofollow",
+      name: 'robots',
+      content: 'noindex, nofollow',
     },
-  ];
-};
+  ]
+}
 
 const RootPage = () => {
-  const params = useParams();
-  const routeFile = params["*"] === undefined ? "routes/_index" : "routes/$";
-  const data = useStoryblokData(routeFile);
+  const params = useParams()
+  const routeFile = params['*'] === undefined ? 'routes/_index' : 'routes/$'
+  const data = useStoryblokData(routeFile)
 
-  return data;
-};
+  return data
+}
 
-export default RootPage;
+export default RootPage
 
 export function ErrorBoundary() {
   return (
@@ -114,5 +114,5 @@ export function ErrorBoundary() {
         404: () => <NotFoundPage />,
       }}
     />
-  );
+  )
 }
